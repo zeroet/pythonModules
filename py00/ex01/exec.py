3dc3a89af984c07b11756d17c6b575e224d1f3f8
zeroet <y36609672@gmail.com>
2023-01-11 16:19:41 +0100

    01

--- py00/ex01/exec.py
+++ py00/ex01/exec.py
@@ -1,15 +1,18 @@
 import sys
 
 test_string = sys.argv
-print(test_string)
+if len(test_string) > 0:
+    test_string = test_string[1::]
+    test_string = ' '.join(test_string)[::-1]
+    print(test_string.upper())
 
 
 #if test_string is not None:
 #if test_string != None:
 
-print(test_string)
+# print(test_string)
 
-for x in test_string:
-    print(x.upper())
+# for x in test_string:
+#     print(x.upper())
 
 
