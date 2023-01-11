8ae79b9f1b37256d2b545c6ccc302f4712095a1a
zeroet <y36609672@gmail.com>
2023-01-11 15:59:46 +0100

    commit

---
+++ py00/ex01/exec.py
@@ -0,0 +1,15 @@
+import sys
+
+test_string = sys.argv
+print(test_string)
+
+
+#if test_string is not None:
+#if test_string != None:
+
+print(test_string)
+
+for x in test_string:
+    print(x.upper())
+
+
